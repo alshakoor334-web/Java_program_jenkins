a541b14d65e09d5668a9959c4c1d01baeaa6b77c
alshakoor334-web <alshakoor334@gmail.com>
2025-11-09 09:03:09 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is Java program");
+
+        System.out.println("output from java program");
+        System.out.println(new Date());
+
+    }
+}
